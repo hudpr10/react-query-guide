860e4ddc37a26fde2882c25452ee3f93770bdc1c
Hudson Reis <hudsonpr09@gmail.com>
2025-09-18 13:54:12 -0300

    criando o query provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const client = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* Criando o React Query Provider */}
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
